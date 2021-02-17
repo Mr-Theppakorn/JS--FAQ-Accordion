9b764e9819feee687f7ad2743d136e8a38bee90b
Mr-Theppakorn <38773611+Mr-Theppakorn@users.noreply.github.com>
2021-02-17 15:14:42 +0700

    add new file

---
+++ JS- FAQ-Accordion/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var accordion = document.getElementsByClassName('accordion-item');
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
+}
